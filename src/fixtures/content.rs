ff2882f7189ba249026f47db663fd446e84e16d9
frolovdev <frolovandrey752@gmail.com>
2022-06-06 00:50:26 +0400

    delete unused comment

--- src/fixtures/content.rs
+++ src/fixtures/content.rs
@@ -1,10 +1,9 @@
 use base64;
 use octocrab::models::repos::{Content, ContentLinks};
 use reqwest::Url;
-use serde_json::json;
 
 pub fn get_content_json(path: &str, name: &str, content: Option<&str>, r#type: &str) -> Content {
-    let kek = Content {
+    Content {
         path: path.to_string(),
         name: name.to_string(),
         sha: "".to_string(),
@@ -21,24 +20,5 @@ pub fn get_content_json(path: &str, name: &str, content: Option<&str>, r#type: &
             _self: Url::parse("https://example.net").unwrap(),
         },
         license: None,
-    };
-
-    kek
-
-    // return json!({
-    //   "path": path,
-    //   "name": name,
-    //   "sha": "",
-    //   "content": base64::encode(content.unwrap_or("")),
-    //   "size": 45,
-    //   "url": "",
-    //   "html_url": "",
-    //   "git_url": "",
-    //   "type": r#type,
-    //   "_links": {
-    //       "git": "https://example.net",
-    //       "html": "https://example.net",
-    //       "self": "https://example.net",
-    //   },
-    // });
+    }
 }
